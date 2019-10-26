875ce930167f2c0a90fbc9846263e0e1a8beadce
homosapien97 <christian.antoni.gutowski@gmail.com>
2019-10-26 00:28:09 -0400

    Fullscreen

--- core/src/com/sleepyheads/game/GTGame.java
+++ core/src/com/sleepyheads/game/GTGame.java
@@ -2,6 +2,7 @@ package com.sleepyheads.game;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL30;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
@@ -15,6 +16,7 @@ public class GTGame extends ApplicationAdapter {
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
+		shapeRenderer = new ShapeRenderer();
 		img = new Texture("badlogic.jpg");
 	}
 
@@ -25,7 +27,10 @@ public class GTGame extends ApplicationAdapter {
 		batch.begin();
 		batch.draw(img, 0, 0);
 		batch.end();
-
+		shapeRenderer.setColor(Color.BLACK);
+		shapeRenderer.begin(ShapeRenderer.ShapeType.Filled);
+		shapeRenderer.circle(500.0f, 50.0f, 20.0f);
+		shapeRenderer.end();
 	}
 	
 	@Override
--- desktop/src/com/sleepyheads/game/desktop/DesktopLauncher.java
+++ desktop/src/com/sleepyheads/game/desktop/DesktopLauncher.java
@@ -7,6 +7,11 @@ import com.sleepyheads.game.GTGame;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.title = "Griphon and Turtle";
+		config.useGL30 = true;
+		config.width = LwjglApplicationConfiguration.getDesktopDisplayMode().width;
+		config.height = LwjglApplicationConfiguration.getDesktopDisplayMode().height;
+		config.fullscreen = true;
 		new LwjglApplication(new GTGame(), config);
 	}
 }
